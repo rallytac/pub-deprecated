8a1e71af2637e5a5d49dd0d77c6f8a773011a768
Shaun Botha <shaun.botha@ucdynamics.com>
2019-11-05 21:18:17 -0800

    Trying out some scripting for testing

---
+++ testing/configure.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function show_usage()
+{
+    echo "usage: configure <platform> <binary_version>"    
+}
+
+PLATFORM=${1}
+VERSION=${2}
+GITHUB_BASE=https://github.com/rallytac/pub/raw/master
+
+if [[ "${PLATFORM}" == "" ]]; then
+    show_usage
+    exit 1
+fi
+
+if [[ "${VERSION}" == "" ]]; then
+    show_usage
+    exit 1
+fi
+
+# Configurations
+wget ${GITHUB_BASE}/configurations/sample_engine_policy.json
+wget ${GITHUB_BASE}/configurations/sample_mission_template.json
+
+# Certificates
+wget ${GITHUB_BASE}/certificates/rtsCA.pem
+wget ${GITHUB_BASE}/certificates/rtsFactoryDefaultEngage.pem
+wget ${GITHUB_BASE}/certificates/rtsFactoryDefaultEngage.key
+
+# Binaries
+wget ${GITHUB_BASE}/bin/${VERSION}/${PLATFORM}/engage-cmd
+chmod +x engage-cmd
+wget ${GITHUB_BASE}/bin/${VERSION}/${PLATFORM}/libengage-shared.so
+chmod +x libengage-shared.so
+
+echo "Dont forget to run 'export LD_LIBRARY_PATH=./' in your terminal!"
