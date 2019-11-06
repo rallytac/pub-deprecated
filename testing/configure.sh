5f8db9b5f2b1de89060a350b71669c66a6b5b566
Shaun Botha <shaun.botha@ucdynamics.com>
2019-11-05 22:12:10 -0800

    wip

--- testing/configure.sh
+++ testing/configure.sh
@@ -19,16 +19,16 @@ if [[ "${VERSION}" == "" ]]; then
     exit 1
 fi
 
+# Certificates
+wget -O rtsCA.pem ${GITHUB_BASE}/certificates/rtsCA.pem
+wget -O rtsFactoryDefaultEngage.pem ${GITHUB_BASE}/certificates/rtsFactoryDefaultEngage.pem
+wget -O rtsFactoryDefaultEngage.key ${GITHUB_BASE}/certificates/rtsFactoryDefaultEngage.key
+
 # Configurations
 wget -O sample_engine_policy.json ${GITHUB_BASE}/configurations/sample_engine_policy.json
 sed -i 's/@..\/certificates\//@.\//g' sample_engine_policy.json
 wget -O sample_mission_template.json ${GITHUB_BASE}/configurations/sample_mission_template.json
 sed -i 's/@..\/certificates\//@.\//g' sample_mission_template.json
-
-# Certificates
-wget -O rtsCA.pem ${GITHUB_BASE}/certificates/rtsCA.pem
-wget -O rtsFactoryDefaultEngage.pem ${GITHUB_BASE}/certificates/rtsFactoryDefaultEngage.pem
-wget -O rtsFactoryDefaultEngage.key ${GITHUB_BASE}/certificates/rtsFactoryDefaultEngage.key
 
 # Binaries
 wget -O engage-cmd ${GITHUB_BASE}/bin/${VERSION}/${PLATFORM}/engage-cmd
