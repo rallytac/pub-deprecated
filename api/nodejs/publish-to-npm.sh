8416c073c52d337c7fa5d0dd8acd764eb3a1fd61
Shaun Botha <shaun.botha@ucdynamics.com>
2019-11-20 11:53:13 -0800

    Fix for NPM publish

--- api/nodejs/publish-to-npm.sh
+++ api/nodejs/publish-to-npm.sh
@@ -9,14 +9,19 @@ THIS_ROOT=`pwd`
 BUILD_ROOT=${THIS_ROOT}/.build
 API_ROOT=${THIS_ROOT}/../
 BIN_ROOT=${THIS_ROOT}/../../bin
-LATEST_BIN_VERSION=""
-VERSION_EXTENSION="${1}"
+SRC_BIN_VERSION="${1}"
+VERSION_EXTENSION="${2}"
 
-function determine_latest_bin_version()
-{
-    FILES=(`ls -r ${BIN_ROOT}`)
-    LATEST_BIN_VERSION=${FILES[0]}
-}
+if [ "${SRC_BIN_VERSION}" == "" -o "${VERSION_EXTENSION}" == "" ]; then
+    echo "usage ${0} src_version pub_extension"
+    exit 1
+fi
+
+#function determine_src_bin_version()
+#{
+#    FILES=(`ls -r ${BIN_ROOT}`)
+#    SRC_BIN_VERSION=${FILES[0]}
+#}
 
 function publish_it()
 {
@@ -36,27 +41,27 @@ function publish_it()
     cp ${API_ROOT}/c/include/* include
 
     mkdir -p lib/darwin.x64
-    cp ${BIN_ROOT}/${LATEST_BIN_VERSION}/darwin_x64/libengage-shared.dylib lib/darwin.x64
+    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/darwin_x64/libengage-shared.dylib lib/darwin.x64
 
     mkdir -p lib/linux.x64
-    cp ${BIN_ROOT}/${LATEST_BIN_VERSION}/linux_centos_x64/libengage-shared.so lib/linux.x64
+    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/linux_x64/libengage-shared.so lib/linux.x64
 
     mkdir -p lib/win32.x64
-    cp ${BIN_ROOT}/${LATEST_BIN_VERSION}/win_x64/engage-shared.dll lib/win32.x64
-    cp ${BIN_ROOT}/${LATEST_BIN_VERSION}/win_x64/engage-shared.lib lib/win32.x64
+    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/win_x64/engage-shared.dll lib/win32.x64
+    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/win_x64/engage-shared.lib lib/win32.x64
 
     mkdir -p lib/win32.ia32
-    cp ${BIN_ROOT}/${LATEST_BIN_VERSION}/win_ia32/engage-shared.dll lib/win32.ia32
-    cp ${BIN_ROOT}/${LATEST_BIN_VERSION}/win_ia32/engage-shared.lib lib/win32.ia32
+    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/win_ia32/engage-shared.dll lib/win32.ia32
+    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/win_ia32/engage-shared.lib lib/win32.ia32
 
-    npm version ${LATEST_BIN_VERSION}${VERSION_EXTENSION}
+    npm version ${SRC_BIN_VERSION}${VERSION_EXTENSION}
     npm publish
 
     cd ${CURRDIR}
     rm -rf ${BUILD_ROOT}
 }
 
-determine_latest_bin_version
-echo "Publishing for version ${LATEST_BIN_VERSION}${VERSION_EXTENSION} ..."
+#determine_SRC_bin_version
+echo "Publishing for version ${SRC_BIN_VERSION}${VERSION_EXTENSION} ..."
 
 publish_it
