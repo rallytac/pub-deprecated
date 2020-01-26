9b021983add30286fadb70f1bbf5d16300ad140b
Shaun Botha <shaun.botha@ucdynamics.com>
2020-01-26 11:19:01 -0800

    1.116.8874 api for C/C++

---
+++ api/nodejs/.build/include/EngageIntegralDataTypes.h
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) 2019 Rally Tactical Systems, Inc.
+//  All rights reserved.
+//
+
+#ifndef EngageIntegralDataTypes_h
+#define EngageIntegralDataTypes_h
+
+typedef unsigned char uint8_t;      ///< Unsigned char (8 bits)
+typedef unsigned short uint16_t;    ///< Unsigned short (16 bits)
+typedef unsigned long uint32_t;     ///< Unsigned long (32 bits)
+typedef unsigned int size_t;        ///< Unsigned integer ()
+
+#endif // EngageIntegralDataTypes_h
+
