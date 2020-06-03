514512bb955375da598415528dcff2934ae028d5
Shaun Botha <shaun.botha@ucdynamics.com>
2020-06-03 14:03:27 -0700

    Updated NPM publisher

--- api/nodejs/publish-to-npm.sh
+++ api/nodejs/publish-to-npm.sh
@@ -33,10 +33,10 @@ function get_microsoft_redistributables()
         PLAT=${1}
         FN=${2}
 
-        if [ ! -f "${WINDOWS_REDIST_ROOT}/${PLAT}/${FN}" ]; then
-            wget "${WINDOWS_REDIST_URL}/${PLAT}/${FN}" -O "${WINDOWS_REDIST_ROOT}/${PLAT}/${FN}" > /dev/null
+        if [ ! -f "${WINDOWS_REDIST_ROOT}/${PLAT}${FN}" ]; then
+            wget "${WINDOWS_REDIST_URL}/${PLAT}${FN}" -O "${WINDOWS_REDIST_ROOT}/${PLAT}${FN}" > /dev/null
             if [[ $? != "0" ]]; then
-                echo "ERROR: Failed to download Windows redistributable ${PLAT}/${FN}"
+                echo "ERROR: Failed to download Windows redistributable ${PLAT}${FN}"
                 exit 1
             fi
         fi
@@ -48,9 +48,62 @@ function get_microsoft_redistributables()
     FILES=("concrt140.dll" "msvcp140.dll" "msvcp140_1.dll" "msvcp140_2.dll" "vccorlib140.dll" "vcruntime140.dll")
 
     for f in ${FILES[@]}; do
-        check_for_ms_file "x64" "${f}"
-        check_for_ms_file "ia32" "${f}"
+        check_for_ms_file "x64/" "${f}"
+        check_for_ms_file "ia32/" "${f}"
     done
+
+    mkdir -p "${WINDOWS_REDIST_ROOT}/x64/ucrt"
+    mkdir -p "${WINDOWS_REDIST_ROOT}/ia32/ucrt"
+
+    FILES=("api-ms-win-core-console-l1-1-0.dll" \
+            "api-ms-win-core-datetime-l1-1-0.dll" \
+            "api-ms-win-core-debug-l1-1-0.dll" \
+            "api-ms-win-core-errorhandling-l1-1-0.dll" \
+            "api-ms-win-core-file-l1-1-0.dll" \
+            "api-ms-win-core-file-l1-2-0.dll" \
+            "api-ms-win-core-file-l2-1-0.dll" \
+            "api-ms-win-core-handle-l1-1-0.dll" \
+            "api-ms-win-core-heap-l1-1-0.dll" \
+            "api-ms-win-core-interlocked-l1-1-0.dll" \
+            "api-ms-win-core-libraryloader-l1-1-0.dll" \
+            "api-ms-win-core-localization-l1-2-0.dll" \
+            "api-ms-win-core-memory-l1-1-0.dll" \
+            "api-ms-win-core-namedpipe-l1-1-0.dll" \
+            "api-ms-win-core-processenvironment-l1-1-0.dll" \
+            "api-ms-win-core-processthreads-l1-1-0.dll" \
+            "api-ms-win-core-processthreads-l1-1-1.dll" \
+            "api-ms-win-core-profile-l1-1-0.dll" \
+            "api-ms-win-core-rtlsupport-l1-1-0.dll" \
+            "api-ms-win-core-string-l1-1-0.dll" \
+            "api-ms-win-core-synch-l1-1-0.dll" \
+            "api-ms-win-core-synch-l1-2-0.dll" \
+            "api-ms-win-core-sysinfo-l1-1-0.dll" \
+            "api-ms-win-core-timezone-l1-1-0.dll" \
+            "api-ms-win-core-util-l1-1-0.dll" \
+            "api-ms-win-crt-conio-l1-1-0.dll" \
+            "api-ms-win-crt-convert-l1-1-0.dll" \
+            "api-ms-win-crt-environment-l1-1-0.dll" \
+            "api-ms-win-crt-filesystem-l1-1-0.dll" \
+            "api-ms-win-crt-heap-l1-1-0.dll" \
+            "api-ms-win-crt-locale-l1-1-0.dll" \
+            "api-ms-win-crt-math-l1-1-0.dll" \
+            "api-ms-win-crt-multibyte-l1-1-0.dll" \
+            "api-ms-win-crt-private-l1-1-0.dll" \
+            "api-ms-win-crt-process-l1-1-0.dll" \
+            "api-ms-win-crt-runtime-l1-1-0.dll" \
+            "api-ms-win-crt-stdio-l1-1-0.dll" \
+            "api-ms-win-crt-string-l1-1-0.dll" \
+            "api-ms-win-crt-time-l1-1-0.dll" \
+            "api-ms-win-crt-utility-l1-1-0.dll" \
+            "ucrtbase.dll")
+
+    for f in ${FILES[@]}; do
+        check_for_ms_file "x64/ucrt/" "${f}"
+        check_for_ms_file "ia32/ucrt/" "${f}"
+    done
+
+    pwd
+    ls -lsa
 }
 
 function publish_it()
@@ -79,14 +132,14 @@ function publish_it()
     cp ${BIN_ROOT}/${SRC_BIN_VERSION}/linux_x64/libengage-shared.so lib/linux.x64
 
     mkdir -p lib/win32.x64
-    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/win_x64/engage-shared.dll lib/win32.x64
-    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/win_x64/engage-shared.lib lib/win32.x64
-    cp "${WINDOWS_REDIST_ROOT}/x64/"* lib/win32.x64
+    cp -r ${BIN_ROOT}/${SRC_BIN_VERSION}/win_x64/engage-shared.dll lib/win32.x64
+    cp -r ${BIN_ROOT}/${SRC_BIN_VERSION}/win_x64/engage-shared.lib lib/win32.x64
+    cp -r "${WINDOWS_REDIST_ROOT}/x64/"* lib/win32.x64
 
     mkdir -p lib/win32.ia32
-    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/win_ia32/engage-shared.dll lib/win32.ia32
-    cp ${BIN_ROOT}/${SRC_BIN_VERSION}/win_ia32/engage-shared.lib lib/win32.ia32
-    cp "${WINDOWS_REDIST_ROOT}/ia32/"* lib/win32.ia32
+    cp -r ${BIN_ROOT}/${SRC_BIN_VERSION}/win_ia32/engage-shared.dll lib/win32.ia32
+    cp -r ${BIN_ROOT}/${SRC_BIN_VERSION}/win_ia32/engage-shared.lib lib/win32.ia32
+    cp -r "${WINDOWS_REDIST_ROOT}/ia32/"* lib/win32.ia32
 
     npm version ${SRC_BIN_VERSION}${VERSION_EXTENSION}
     npm publish
